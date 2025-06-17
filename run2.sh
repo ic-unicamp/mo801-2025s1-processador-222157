79d5f2533c9bd43da9e282da59a74c4341516d3b
Rodolfo Azevedo <rodolfo.azevedo@unicamp.br>
2025-06-17 15:17:20 -0300

    adicionados testes novos para correção.

--- run2.sh
+++ run2.sh
@@ -9,14 +9,14 @@ if ! iverilog -o tb *.v; then
 fi
 
 cp test/teste$1.mem memory.mem
-./tb > saida.out
-sed -i '' -n '/^M\[/p' saida.out
+vvp tb > saida.out
+sed -i '/M\[/!d' saida.out test/saida$1.ok
+sed -i 's/\r//g' saida.out test/saida$1.ok
 cp saida.out test/saida$1.out
 cp saida.vcd test/saida$1.vcd
 rm saida.out saida.vcd memory.mem
 
-if diff test/saida$1.out test/saida$1.ok >/dev/null; then
-    echo "OK"
+if diff --strip-trailing-cr test/saida$1.out test/saida$1.ok >/dev/null; then    echo "OK"
     exit 0
 else
     echo "ERRO"
